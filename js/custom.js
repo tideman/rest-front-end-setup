52f07cb44468af8b703078c975a98e55e23e9461
Tijmen Kroon <t.kroon@lukkien.com>
2014-11-14 09:54:15 +0100

    add basic auth

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,16 @@
  * Created by tkroon on 11/12/14.
  */
 (function() {
+    var rest_request,
+        json_response;
+
+    $.ajaxSetup({
+        beforeSend: function (jqXHR, settings) {
+            jqXHR.setRequestHeader('Authorization', 'Basic ' + 'dXJjLWFkbWluOnVpYWIuYWRtaW4x');
+            return true;
+        }
+    });
+
 
     $('#getcustomer').on('click', function () {
         $.getJSON('/json/show_user.json', function(data) {
