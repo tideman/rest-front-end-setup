6e62b2ec17a9e04a6a403db5b9f0585a2001aeed
Tijmen Kroon <t.kroon@lukkien.com>
2014-11-16 16:08:49 +0100

    changed showcustomer form to meveo json

--- js/custom.js
+++ js/custom.js
@@ -2,23 +2,13 @@
  * Created by tkroon on 11/12/14.
  */
 (function() {
-    var rest_request,
-        json_response;
-
-    $.ajaxSetup({
-        beforeSend: function (jqXHR, settings) {
-            jqXHR.setRequestHeader('Authorization', 'Basic ' + 'dXJjLWFkbWluOnVpYWIuYWRtaW4x');
-            return true;
-        }
-    });
-
 
     $('#getcustomer').on('click', function () {
-        $.getJSON('/json/show_user.json', function(data) {
+        $.getJSON('/json/show_customer.json', function(data) {
             var template, html;
             template = $('#customer-form').html();
             console.log('asdas' + data);
-            html = Mustache.render(template, data);
+            html = Mustache.render(template, data.customerAccountDto);
             console.log(html);
             $('#content-section').html(html);
         });
---
+++ json/show_invoice.json.js
@@ -0,0 +1,51 @@
+{
+    "Invoice": {
+    "requestId": "...",
+        "invoiceNumber": "...",
+        "billingAccountCode": "...",
+        "invoiceDate": "...",
+        "dueDate": "...",
+        "discount": "...",
+        "amountWithoutTax": "...",
+        "amountTax": "...",
+        "amountWithTax": "...",
+        "paymentMathod": "...",
+        "PDFpresent": "...",
+        "subCategoryInvoiceAgregates": [
+        {
+            "type": "...",
+            "itemNumber": "...",
+            "accountingCode": "...",
+            "description": "...",
+            "taxPercent": "...",
+            "quantity": "...",
+            "discount": "...",
+            "amountWithoutTax": "...",
+            "amountTax": "...",
+            "amountWithTax": "...",
+            "ratedTransactions": [
+                {
+                    "requestId": "...",
+                    "usageDate": "...",
+                    "unitAmountWithoutTax": "...",
+                    "unitAmountWithTax": "...",
+                    "unitAmountTax": "...",
+                    "quantity": "...",
+                    "amountWithoutTax": "...",
+                    "amountWithTax": "...",
+                    "amountTax": "...",
+                    "code": "...",
+                    "description": "...",
+                    "doNotTriggerInvoicing": "..."
+                },
+                {
+
+                }
+            ]
+        },
+        {
+
+        }
+    ]
+}
+}
