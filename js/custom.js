a85c81c100008cb6730a66c19e6e02e11ebccc68
Tijmen Kroon <t.kroon@lukkien.com>
2014-11-16 16:34:31 +0100

    added show invoice

--- js/custom.js
+++ js/custom.js
@@ -2,18 +2,22 @@
  * Created by tkroon on 11/12/14.
  */
 (function() {
-
     $('#getcustomer').on('click', function () {
         $.getJSON('/json/show_customer.json', function(data) {
             var template, html;
             template = $('#customer-form').html();
-            console.log('asdas' + data);
             html = Mustache.render(template, data.customerAccountDto);
-            console.log(html);
             $('#content-section').html(html);
         });
+    });
 
-
+    $('#getinvoice').on('click', function () {
+        $.getJSON('/json/show_invoice2.json', function(data) {
+            var template, html;
+            template = $('#invoice-form').html();
+            html = Mustache.render(template, data.CustomerInvoiceDtoList[0]);
+            $('#content-section').html(html);
+        });
     });
 
 })();
